d26c520011e65cf84a355abcc717ecad917dff75
Nitya Rathod <nityarathod5@gmail.com>
2026-07-02 22:41:57 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // middlewares
 app.use(
