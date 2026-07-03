c55add1ca19e6d4062d48508c7c312d2bc0e3eac
Nitya Rathod <nityarathod5@gmail.com>
2026-07-03 11:49:25 +0530

    Fix logout controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,7 +36,6 @@ export const signup = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -101,7 +100,7 @@ export const logout = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: isProduction ? "none" : "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   });
 
   res.status(200).json({
